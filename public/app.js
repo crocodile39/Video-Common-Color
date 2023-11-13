35b56e6c59f8b03ca5834e13289691c06fa211b9
crocodile39 <121732018+crocodile39@users.noreply.github.com>
2023-11-12 22:35:56 -0500

    Update app.js: makes any dark objects become black.

--- public/app.js
+++ public/app.js
@@ -12,27 +12,29 @@ navigator.mediaDevices.getUserMedia({ video: true })
 
 video.addEventListener('canplay', () => {
   setInterval(() => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height).data;
 
-    const colorOccurrences = {};
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 
-    for (let i = 0; i < imageData.length; i += 4) {
-      const color = `rgb(${imageData[i]}, ${imageData[i + 1]}, ${imageData[i + 2]})`;
+    applyDarkeningEffect(imageData, 90); 
 
-      colorOccurrences[color] = (colorOccurrences[color] || 0) + 1;
-    }
-
-    let mostCommonColor = null;
-    let maxOccurrences = 0;
-
-    for (const color in colorOccurrences) {
-      if (colorOccurrences[color] > maxOccurrences) {
-        maxOccurrences = colorOccurrences[color];
-        mostCommonColor = color;
-      }
-    }
-
-    document.body.style.backgroundColor = mostCommonColor || 'white';
-  }, 2000); 
+    context.putImageData(imageData, 0, 0);
+  }, 100); 
 });
+
+function applyDarkeningEffect(imageData, threshold) {
+  const pixels = imageData.data;
+
+  for (let i = 0; i < pixels.length; i += 4) {
+    const brightness = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
+
+    if (brightness < threshold) {
+      const newBrightness = Math.max(0, brightness - 50); 
+
+      pixels[i] = newBrightness;
+      pixels[i + 1] = newBrightness;
+      pixels[i + 2] = newBrightness;
+    }
+  }
+}
